c6c0f5a6a97721e493a1962c9560a21a1f7d7fa1
Edsamn <eduardomsribas@gmail.comm>
2024-01-17 13:52:41 -0300

    identação

--- revisao_marcele/ex4/scripts.js
+++ revisao_marcele/ex4/scripts.js
@@ -18,33 +18,40 @@
 // Nome: Alessandro, Idade: 28, Salário: 2700
 // Nome: Alessandro, Idade: 28, Salário: 3000
 
-let desempregadas = []
-let salarioMenor2500 = []
-let salarioMaior2500 = []
+let desempregadas = [];
+let salarioMenor2500 = [];
+let salarioMaior2500 = [];
 
-let pessoa = {}
+let pessoa = {};
 
 do {
-    var pessoaNova = {...pessoa}
-    pessoaNova.nome = String(prompt('Digite o nome'))
-    pessoaNova.idade = Number(prompt('Digite a idade'))
-    pessoaNova.trabalho = (confirm('Está trabalhando?'))
-if (pessoaNova.trabalho === true) {
-    pessoaNova.salario = Number(prompt('Digite o salario')) 
-}
+  var pessoaNova = { ...pessoa };
+  pessoaNova.nome = String(prompt("Digite o nome"));
+  pessoaNova.idade = Number(prompt("Digite a idade"));
+  pessoaNova.trabalho = confirm("Está trabalhando?");
+  if (pessoaNova.trabalho === true) {
+    pessoaNova.salario = Number(prompt("Digite o salário"));
+  }
 
-if (pessoaNova.trabalho === false) {
-    desempregadas.push(pessoaNova)
-} else if (pessoaNova.trabalho === true && pessoaNova.salario > 0 && pessoaNova.salario <= 2500) {
-    salarioMenor2500.push(pessoaNova)
-} else {
-    salarioMaior2500.push(pessoaNova)
-}
-} while (confirm('Deseja continuar?') === true);
+  if (pessoaNova.trabalho === false) {
+    desempregadas.push(pessoaNova);
+  } else if (
+    pessoaNova.trabalho === true &&
+    pessoaNova.salario > 0 &&
+    pessoaNova.salario <= 2500
+  ) {
+    salarioMenor2500.push(pessoaNova);
+  } else {
+    salarioMaior2500.push(pessoaNova);
+  }
+} while (confirm("Deseja continuar?") === true);
 
-console.log("Pessoas desempregadas:", 
-desempregadas)
-console.log("Pessoas empregadas com salários menores que 2500:" , 
-salarioMenor2500)
-console.log("Pessoas empregadas com salários maiores que 2500:" , 
-salarioMaior2500)
+console.log("Pessoas desempregadas:", desempregadas);
+console.log(
+  "Pessoas empregadas com salários menores que 2500:",
+  salarioMenor2500
+);
+console.log(
+  "Pessoas empregadas com salários maiores que 2500:",
+  salarioMaior2500
+);
